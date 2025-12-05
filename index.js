8de5a859d9209ddc2ed5470ce34ddcd7b486f81b
idazanggara <idazideas@gmail.com>
2025-12-05 21:32:21 +0700

    add feature index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+// sadfasdf, asdfasdfm,as .asfdasdf,asdfasd,,asdf
+// asdfasdfasd
+// asdfasdfasd
+// asdfasdfasdf
+// asdfasdfadsf
+// a
+
+
+var sebuahVariable = 10
+sebuahVariable = "data string"
+
+
+// const sebuahArray = [
+//   0:1,
+//   1:2,
+//   2:3,
+//   3:4,
+//   4:5
+// ]
+const sebuahArray = [
+  1,
+  2,
+  3,
+  4,
+  5
+]
+const sebuahObject = {
+  nama: "John Doe",
+  umur: 30
+}
+
+console.log(3 === "3") //
+
+
+function tambah(a, b) {
+  if (!(sebuahVariable === 10) && (sebuahObject.umur === 30)) {
+    console.log("nilai sebuahVariable adalah 10")
+  } else {
+    console.log("nilai sebuahVariable bukan 10")
+  }
+}
+
+
+
+
+
+
+
